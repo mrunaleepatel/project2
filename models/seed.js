c8f15b657618338792182a516fa4e3a0639d2a32
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-07-10 10:33:42 -0500

    fixed seed

--- models/seed.js
+++ models/seed.js
@@ -2,13 +2,13 @@ const mongoose = require('./connection');
 const Weather = require('./weather');
 
 mongoose.connection.on('open', async () => {
-    await weather.deleteMany();
-    const weather = [
+    await Weather.deleteMany();
+    const weatherData = [
         { location: 'New York', activity: 'Sightseeing', temperature: 75, windSpeed: 10, precipitation: 'Sunny', toGo: true  }, 
         { location: 'London', activity: 'Shopping', temperature: 62, windSpeed: 5, precipitation: 'Rainy', toGo: true },
         { location: 'Chicago', activity: 'Hiking', temperature: 80, windSpeed: 15, precipitation: 'Rainy', toGo: true },
     ];
-    await weather.create(weather);
+    await Weather.create(weatherData);
 
     mongoose.connection.close();
 });
